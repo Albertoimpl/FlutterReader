f32fb112fb85c2c86da519930e00aac5ac03cfd9
Keshava11 <mfsi.ravi@gmail.com>
2018-05-02 13:26:58 +0530

    Updated dependencies and apis. Added support to make it run as android application

--- lib/reddit_list.dart
+++ lib/reddit_list.dart
@@ -1,20 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:FlutterReader/reddit_data.dart';
+import 'package:FlutterReader/reddit_row.dart';
 
-part of reddit;
-
-class RedditList extends StatelessComponent {
+class RedditList extends StatelessWidget {
   RedditList({Key key, this.reddits});
 
   final List<Reddit> reddits;
 
   Widget build(BuildContext context) {
-    return new ScrollableList<Reddit>(
-        items: reddits,
-        itemExtent: RedditRow.kHeight,
-        itemBuilder: (BuildContext context, Reddit reddit, int index) {
-          return new RedditRow(
-              reddit: reddit
-          );
-        }
+    return ListView.builder(
+      padding: new EdgeInsets.all(8.0),
+      itemExtent: RedditRow.kHeight,
+      itemCount: reddits.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new RedditRow(reddit: reddits[index]);
+      },
     );
   }
 }
